01488842a433bbb09223dfbf1b85bf70fd543d51
maliayyildiz <maliayyildiz61@gmail.com>
2020-10-02 01:51:07 +0200

    3rd update

---
+++ src/test/java/com/techproed/pages/HotelRoomsPage.java
@@ -0,0 +1,62 @@
+package com.techproed.pages;
+
+import com.techproed.utilities.TestBase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class HotelRoomsPage extends TestBase{
+    WebDriver driver;
+    public HotelRoomsPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+    //hotel mamagment  hotel room
+    @FindBy(xpath = "//span[@class='hidden-480']")
+    public WebElement addHotelRoomsButton;
+
+    @FindBy(id = "IDHotel")
+    public WebElement idHotel;
+
+    @FindBy (id = "Code")
+    public WebElement hotelCode;
+
+    @FindBy (id = "Name")
+    public WebElement hotelName;
+
+    @FindBy (id = "Location")
+    public WebElement location;
+
+    @FindBy (xpath = "//textarea[@role='textbox']")
+    public WebElement note;
+
+    @FindBy (id = "Price")
+    public WebElement price;
+
+    @FindBy (id = "IDGroupRoomType")
+    public WebElement roomType;
+
+    @FindBy (id = "MaxAdultCount")
+    public WebElement adultCount;
+
+    @FindBy (id = "MaxChildCount")
+    public WebElement childCount;
+
+    @FindBy (id = "IsAvailable")
+    public WebElement availableCheckBox;
+
+    @FindBy (id="btnSubmit")
+    public WebElement submitButton;
+
+    @FindBy (xpath = "//div[@class='bootbox-body']")
+    public WebElement successMessage;
+
+
+
+
+
+
+
+}
